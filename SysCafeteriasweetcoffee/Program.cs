d08ec1c13d333fad2a5110218230e8c0ec2661e7
ericka.segura05 <ericka.segura05@hotmail.com>
2024-10-04 16:47:46 -0600

    Cambios de String de conexión, ya cambíe el string por el se somee y funcionan las entidades solo usuario no.

--- SysCafeteriasweetcoffee/Program.cs
+++ SysCafeteriasweetcoffee/Program.cs
@@ -1,12 +1,17 @@
+using Microsoft.EntityFrameworkCore;
 using SysCafeteriasweetcoffee.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<BDContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<BDContext>();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
